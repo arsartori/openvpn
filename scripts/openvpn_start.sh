265394eb0f1c265efcdea00d5713d8e9c65dbcc1
André Sartori <49533777+arsartori@users.noreply.github.com>
2025-02-01 19:06:54 -0300

    Ajuste genkey

--- scripts/openvpn_start.sh
+++ scripts/openvpn_start.sh
@@ -4,6 +4,6 @@ mkdir -p /dev/net
 mknod /dev/net/tun c 10 200
 echo "Starting OpenVPN Server"
 if [ ! -e $OPENVPN/certs/ta.key ]; then
-    openvpn --genkey --secret $OPENVPN/certs/ta.key
+    openvpn --genkey secret $OPENVPN/certs/ta.key
 fi
 exec openvpn --config $OPENVPN/openvpn.conf
